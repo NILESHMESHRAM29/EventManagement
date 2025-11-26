eda2d8b18ed064f2bc7579c09445875a5fc86b3c
NILESHMESHRAM29 <nsmeshram29@gmail.com>
2025-11-27 00:19:13 +0530

    create remaining controller

---
+++ EventManagement/Controllers/PermissionRolesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventManagement.Data;
+using EventManagement.Models;
+
+namespace EventManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionRolesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PermissionRolesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PermissionRoles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PermissionRole>>> GetPermissionRoles()
+        {
+            return await _context.PermissionRoles.ToListAsync();
+        }
+
+        // GET: api/PermissionRoles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PermissionRole>> GetPermissionRole(int id)
+        {
+            var permissionRole = await _context.PermissionRoles.FindAsync(id);
+
+            if (permissionRole == null)
+            {
+                return NotFound();
+            }
+
+            return permissionRole;
+        }
+
+        // PUT: api/PermissionRoles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPermissionRole(int id, PermissionRole permissionRole)
+        {
+            if (id != permissionRole.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(permissionRole).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PermissionRoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PermissionRoles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PermissionRole>> PostPermissionRole(PermissionRole permissionRole)
+        {
+            _context.PermissionRoles.Add(permissionRole);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPermissionRole", new { id = permissionRole.Id }, permissionRole);
+        }
+
+        // DELETE: api/PermissionRoles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePermissionRole(int id)
+        {
+            var permissionRole = await _context.PermissionRoles.FindAsync(id);
+            if (permissionRole == null)
+            {
+                return NotFound();
+            }
+
+            _context.PermissionRoles.Remove(permissionRole);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PermissionRoleExists(int id)
+        {
+            return _context.PermissionRoles.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EventManagement/Controllers/SectionVolunteersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventManagement.Data;
+using EventManagement.Models;
+
+namespace EventManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectionVolunteersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SectionVolunteersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SectionVolunteers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SectionVolunteer>>> GetSectionVolunteers()
+        {
+            return await _context.SectionVolunteers.ToListAsync();
+        }
+
+        // GET: api/SectionVolunteers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SectionVolunteer>> GetSectionVolunteer(int id)
+        {
+            var sectionVolunteer = await _context.SectionVolunteers.FindAsync(id);
+
+            if (sectionVolunteer == null)
+            {
+                return NotFound();
+            }
+
+            return sectionVolunteer;
+        }
+
+        // PUT: api/SectionVolunteers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSectionVolunteer(int id, SectionVolunteer sectionVolunteer)
+        {
+            if (id != sectionVolunteer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sectionVolunteer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SectionVolunteerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SectionVolunteers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SectionVolunteer>> PostSectionVolunteer(SectionVolunteer sectionVolunteer)
+        {
+            _context.SectionVolunteers.Add(sectionVolunteer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSectionVolunteer", new { id = sectionVolunteer.Id }, sectionVolunteer);
+        }
+
+        // DELETE: api/SectionVolunteers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSectionVolunteer(int id)
+        {
+            var sectionVolunteer = await _context.SectionVolunteers.FindAsync(id);
+            if (sectionVolunteer == null)
+            {
+                return NotFound();
+            }
+
+            _context.SectionVolunteers.Remove(sectionVolunteer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SectionVolunteerExists(int id)
+        {
+            return _context.SectionVolunteers.Any(e => e.Id == id);
+        }
+    }
+}
